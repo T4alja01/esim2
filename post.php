055823571f8a81281d46f658acab5238abc06614
Alonen <t4alja01@students.oamk.fi>
2016-10-31 10:26:20 +0200

    post lomake valmiiksi

--- post.php
+++ post.php
@@ -1,3 +1,24 @@
-<h1>Testataan posh-metodia</h1>
+<h1>Testataan post-metodia</h1>
+<form method="post" action="post.php">
+<label>Etunimi</label>
+<input type="text" name="en" required>
+<br>
+<label>Sukunimi</label>
+<input type="text" name="sn" required>
+<br>
+<input type="submit" name="nappi" value="lähetä">
+</form>
+
+<?php
+if(isset($_POST['nappi']))
+{
+	$enimi=$_POST['en'];
+	$snimi=$_POST['sn'];
+	echo "Terve $enimi $snimi";
+}
+?>
+
+
+
 
 <p>text</p>
